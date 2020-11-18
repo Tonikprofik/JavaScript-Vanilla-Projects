970c8c62e1c8f369446494953d4097f14bc391f6
PonikTrofik <izanami369@gmail.com>
2020-11-18 23:14:18 +0100

    .

---
+++ Menu Slider, Burger, Modal/script.js
@@ -0,0 +1,13 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//toggle nav
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+// show modal
+open.addEventListener()
