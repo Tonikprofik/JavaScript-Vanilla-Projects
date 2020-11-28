4765715c8ea23183d52a241a3ed3a0b1e64b3deb
PonikTrofik <izanami369@gmail.com>
2020-11-28 22:10:36 +0100

    mvp

--- Infinite Scroll/script.js
+++ Infinite Scroll/script.js
@@ -3,7 +3,7 @@ const postsContainer = document.getElementById('posts-container'),
         filter = document.getElementById('filter');
 
 
-let limit = 3;
+let limit = 4;
 let page = 1;
 
 async function getPosts() {
@@ -36,5 +36,51 @@ async function showPosts() {
     //console.log(posts);
 }
 
+
+// Show loader, fetch more posts
+function showLoading() {
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        },300);
+
+    },1000);
+}
+
+// Filter posts by input
+function filterPosts(e) {
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll('.post');
+
+    posts.forEach(post => {
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+        
+        if(title.indexOf(term) > -1  || body.indexOf(term) > -1) {
+            post.style.display = 'flex';
+        } else {
+            post.style.display = 'none';
+        }
+    });
+}
+
+
 // Show initial psots
 showPosts();
+
+window.addEventListener('scroll', () => {
+    //Destructuring
+    const { scrollTop, scrollHeight, clientHeight}  = document.documentElement;
+
+    if(scrollTop + clientHeight >= scrollHeight -5) {
+        showLoading();
+    }
+});
+
+
+filter.addEventListener('input', filterPosts);
